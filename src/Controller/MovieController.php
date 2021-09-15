c8ff3d345e35f863ab5815f2f79430164aa90be0
axel bruneaux <gazhrot@gmail.com>
2021-09-15 18:16:56 +0200

    creation page detail & update page accueil, ajout 2 method dans le controller pour recuperer tout les movies & recuperer seulement 1 movie par son id

--- src/Controller/MovieController.php
+++ src/Controller/MovieController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\Movie;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -13,8 +14,22 @@ class MovieController extends AbstractController
      */
     public function index(): Response
     {
+        $movies = $this->getDoctrine()->getRepository('App:Movie')->findAll();
+
         return $this->render('movie/index.html.twig', [
-            'controller_name' => 'MovieController',
+            'movies' => $movies,
+        ]);
+    }
+
+    /**
+     * @Route("/movie/{id}", name="movie_detail")
+     */
+    public function detail(String $id): Response 
+    {
+        $movie = $this->getDoctrine()->getRepository('App:Movie')->findOneBy(['id_movie' => $id]);
+
+        return $this->render('movie/detail.html.twig', [
+            'movie' => $movie,
         ]);
     }
 }
